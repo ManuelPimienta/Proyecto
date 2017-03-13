6fbf5715f4f6cd1f00103d03e26c2d7085d3ad53
Unknown <mepj@outlook.com>
2017-03-13 11:38:05 -0500

    Configuración vista blog

--- misitio/blog/models.py
+++ misitio/blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+                                            .filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -16,9 +22,20 @@ class Post(models.Model):
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length = 10, choices = STATUS_CHOICES, default = 'draft')
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                            self.publish.strftime('%m'),
+                            self.publish.strftime('%d'),
+                            self.slug])
+
+     
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()
 
