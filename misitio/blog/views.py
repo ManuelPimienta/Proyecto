87e597948c0bcdb8b8c3013e83f8d9eb214aca56
Unknown <mepj@outlook.com>
2017-03-14 09:22:12 -0500

    Paginación

--- misitio/blog/views.py
+++ misitio/blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 post en cada página
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Si la pagina no es entero libera la primera 
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Si la pagina esta fuera de rango libera la ultima 
+        posts = paginator.page(paginator.num_pages)
+        
     return render(request, 'blog/post/list.html',
-                    {'posts': posts})
+                    {'page': page,
+                    'posts': posts})
+    # posts = Post.published.all()
+    # return render(request, 'blog/post/list.html',
+    #                 {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -14,4 +30,5 @@ def post_detail(request, year, month, day, post):
                                     publish__day=day)
 
     return render(request, 'blog/post/list.html',
-                    {'post': post})
+                    {'page': page,
+                    'posts': posts})    
